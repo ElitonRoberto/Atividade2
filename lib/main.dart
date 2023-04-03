fcc8ffa350493d4067220c1e26682002d09d7dc2
ElitonRoberto <elitongt45@gmail.com>
2023-04-03 02:41:59 -0300

    Atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(Aula4());
 }
 
-class Aula4 extends StatelessWidget {//extends: vai consumir de uma classe StatelesWidget
+class Aula4 extends StatelessWidget {
+  //extends: vai consumir de uma classe StatelesWidget
   @override
-  Widget build(BuildContext) {//widget build: construção de varios widget
+  Widget build(BuildContext) {
+    //widget build: construção de varios widget
     return MaterialApp(
-
-      theme: ThemeData(//modifica o tema do appbar
+      /*theme: ThemeData(//modifica o tema do appbar
         primarySwatch: Colors.red,
         textTheme: const TextTheme (bodyMedium: TextStyle(
           fontFamily: "Arial",
           fontSize: 16
         )
         )
-      ),
+      ),Alterar tema*/
 
-      home: Scaffold( 
-        appBar: AppBar(//barra de cima da aplicação
-          title: const Text("Inicio"),
-          centerTitle: true, 
-        ),
+      home: Scaffold(
+          appBar: AppBar(
+            //barra de cima da aplicação
+            title: const Text("Cidades de Rondônia"),
+            centerTitle: true,
+          ),
+          body: Center(
+            //child: Text("Aula", style: TextStyle(fontSize: 25),),
 
-       /*2 body: Center(
-          //child: Text("Aula", style: TextStyle(fontSize: 25),),
-         
-         /*1 child: Row(//para apenas um componente(widget). row: dentro da mesma linha
+            /*1 child: Row(//para apenas um componente(widget). row: dentro da mesma linha
             children: [//conjunto de componentes, usando a virgula para separar os elementos
                 Text("Primeiro texto"),
                 Text("Primeiro texto"),
@@ -36,27 +36,74 @@ class Aula4 extends StatelessWidget {//extends: vai consumir de uma classe State
             ],
             ),1*/
 
-          child: Column(//dentro da mesma coluna
-            children: [//conjunto de componentes
-                Image(image: AssetImage("assets/ji-parana.jpg")),//imagem
-                Icon(Icons.star, color: Colors.blue),//icone
-                Icon(Icons.star, color: Colors.blue),
-                Icon(Icons.star, color: Colors.blue),
+            child: Column(
+              //dentro da mesma coluna
+              children: [
+                //conjunto de componentes
+                Image(image: AssetImage("assets/ji-parana.jpg")), //imagem
+
                 Container(
-                  padding: EdgeInsets.all(15),//espaço de 15px entre os componentes
-                  child: Row(
+                  padding:
+                      EdgeInsets.all(15), //espaço de 15px entre os componentes
+                  child: const Row(
                     children: [
-                      Text("Brasil"),
-                      Text("Ji-Paraná, RO"),
+                      Text(
+                        "Brasil",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+
+                      Icon(Icons.star, color: Colors.blue), //icone
+                      Text("3500"),
                     ],
-                  ),
+                  ), //termina a linha
                 ),
-            ],
-            )
 
-        )2*/
+               Text("Ji-Paraná, RO"),
 
-        /*3body: ListView(//cria uma lista 
+                Icon(Icons.call, color: Colors.blue),
+                Text(
+                  "Ligar",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+
+                Icon(Icons.location_city, color: Colors.blue),
+                Text(
+                  "Endereço",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+
+                Icon(Icons.share, color: Colors.blue),
+                Text(
+                  "Compartilhar",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+
+                // Icon(Icons.star, color: Colors.blue),
+                //Text("Ji-Paraná é um município brasileiro do estado de Rondônia. Sua população, conforme estimativas do IBGE de 2021, era de 131 026 habitantes,[5] sendo o segundo mais populoso do estado e o décimo sexto mais populoso da Região Norte do Brasil, a 226ª mais populosa do Brasil e a 113ª mais populosa cidade do interior brasileiro.")
+              ],
+            ),
+
+            /*GridView.count(//Divisão em campos
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 3,
+              children: [
+                Container(
+                  color: Colors.yellow
+                ),
+                Container(
+                  color: Colors.green
+                ),
+                Container(
+                  color: Colors.blue
+                ),
+               /* Container(
+                  color: Colors.red
+                )*/
+              ],
+          ),*/
+          )
+
+          /*3body: ListView(//cria uma lista 
               children:[
                 Container(//container: vai englobar uma serie de layout
                   height: 120,
@@ -86,8 +133,7 @@ class Aula4 extends StatelessWidget {//extends: vai consumir de uma classe State
               ]
             )3*/
 
-
-          /*4 body: GridView.count(
+          /*4 body: GridView.count(//Divisão em campos
               scrollDirection: Axis.vertical,
               crossAxisCount: 2,
               children: [
@@ -106,7 +152,7 @@ class Aula4 extends StatelessWidget {//extends: vai consumir de uma classe State
               ],
           )4*/
 
-          /*5 drawer: Drawer(
+          /*5 drawer: Drawer( //menu lateral
             child: ListView(
               children:[
                 DrawerHeader(
@@ -122,10 +168,8 @@ class Aula4 extends StatelessWidget {//extends: vai consumir de uma classe State
               ]
             )
           )5**/
-      
 
-
-      ),
+          ),
     );
   }
 }
